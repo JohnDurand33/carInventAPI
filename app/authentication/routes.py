eab1f48bab0dc1b65f0a5a5026bb5e67ac688793
JohnDurand33 <142426904+JohnDurand33@users.noreply.github.com>
2024-02-13 07:23:20 -0700

    Updates

---
+++ app/authentication/routes.py
@@ -0,0 +1,7 @@
+from flask import UserLoginForm
+from models import User, db, check_password_hash
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+
+from flask_login import login_user, logout_user, LoginManager, current_user, login_required
+
+auth = Blueprint('auth', template_folder='auth_templates')
