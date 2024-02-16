a6e359e2979846c7f27c540b4c76c58f2f1a4589
JohnDurand33 <142426904+JohnDurand33@users.noreply.github.com>
2024-02-16 10:46:11 -0700

    updates

--- app/authentication/routes.py
+++ app/authentication/routes.py
@@ -2,13 +2,13 @@ from forms import UserLoginForm
 from models import User, db, check_password_hash
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, LoginManager, current_user, login_required
+import models
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
 @auth.route('/signup', methods=['GET','POST']) 
 def signup():
     form = UserLoginForm()
-
 
     if request.method == 'POST' and form.validate_on_submit():
         email = form.email.data
@@ -36,9 +36,11 @@ def signin():
             print(f' email | {email}, password | {password}')
 
             logged_user = User.query.filter(User.email == email).first()
+            print(f'logged_user: {logged_user}')
+            
             if logged_user and check_password_hash(logged_user.password, password):
                 login_user(logged_user)
-                print('You were successful in your login')
+                print('You wereflask run successful in your login')
                 return redirect(url_for('site.profile'))
             else:
                 print('You have failed to login (security!)', 'auth-failed')
--- migrations/versions/0f5d21c7e2e4_.py
+++ migrations/versions/0f5d21c7e2e4_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 84634bf85f4e
+Revision ID: 0f5d21c7e2e4
 Revises: 
-Create Date: 2024-02-16 08:59:26.718805
+Create Date: 2024-02-16 10:35:31.742224
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '84634bf85f4e'
+revision = '0f5d21c7e2e4'
 down_revision = None
 branch_labels = None
 depends_on = None
